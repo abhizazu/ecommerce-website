fee642ab10868260f15afbf1dd369364c3a24265
abhizazu <abhi143el@gamil.com>
2024-12-12 16:49:36 +0530

    it my check

--- backend/app.py
+++ backend/app.py
@@ -44,3 +44,6 @@ def get_orders():
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
